6355ae677257e85c36b0d64126582c7430cd70e4
bunyod zaripov <zaripovbunyod571@gmail.com>
2023-12-25 11:24:39 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const inputBox = document.querySelector("#input-text")
+const listContainer = document.querySelector("#list-container")
+const btn = document.querySelector("button")
+
+
+btn.addEventListener('click', () => {
+    if (inputBox.value === '') {
+        alert("you must write something!")
+    } else {
+        toDo()
+    }
+    inputBox.value = ''
+})
+
+function toDo () {
+    const li = document.createElement("li")
+    li.innerHTML = inputBox.value
+    listContainer.appendChild(li)
+
+    const span = document.createElement("span")
+    span.innerHTML = "\u00d7"
+    li.appendChild(span)
+    saveData()
+}
+
+listContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        saveData()
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove()
+        saveData()
+    }
+})
+
+function saveData () {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showData () {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showData()
